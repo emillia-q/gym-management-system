df16889af0d51f590d8086732932ea8bb4678564
artkorzh228 <aleksandr.k.197762@gmail.com>
2026-01-17 22:00:18 +0100

    Add Finance feature(osoba 3)

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+
 from .database import engine, Base # Import connection tools
 from . import models
+from . import finance_models  # важно: зарегистрировать новые таблицы в metadata
+from .finance_router import router as finance_router
 
 # Check models and create tables in the DB
-# If tables already exist don't overwrite them, only ccreate new ones
+# If tables already exist don't overwrite them, only create new ones
 models.Base.metadata.create_all(bind=engine)
 
 # Instance of FastAPI class
 app=FastAPI()
-
+app.include_router(finance_router)
 # Tells which URL should trigger this function
 # The one below means: when someone visits the home page...
 @app.get("/")
